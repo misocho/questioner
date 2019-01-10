13edb9da255840526fafd2089583ff8e92399802
misocho <misochobrian@gmail.com>
2019-01-10 20:19:00 +0300

    [start #163038937] adds upvote question model

--- app/api/v1/models/question_model.py
+++ app/api/v1/models/question_model.py
@@ -1,5 +1,6 @@
 questions = []
 
+
 class QuestionModels():
     def __init__(self):
         self.db = questions
@@ -8,12 +9,25 @@ class QuestionModels():
     def post_question(self, meetup_id, postedby, body):
         """ method to post question """
         payload = {
-            "meetup" : meetup_id,
-            "question_id" : str(len(questions) + 1),
-            "postedby" : postedby, 
-            "body" : body,
-            "votes" : self.votes
+            "meetup": meetup_id,
+            "question_id": str(len(questions) + 1),
+            "postedby": postedby,
+            "body": body,
+            "votes": self.votes
         }
 
         self.db.append(payload)
-        return payload, {"message" : "question was successsfully posted"}
+        return payload, {"message": "question was successsfully posted"}
+
+    def upvote_question(self, question_id):
+        """ method to upvote question """
+        question = [
+            question for question in self.db if question["question_id"] == question_id]
+        if question:
+            payload = {
+                "meetupId": question[0]["meetup"],
+                "body" : question[0]["body"],
+                "votes" : self.votes + 1
+                }
+
+            return payload, {"message" : "upvote successfull"}
