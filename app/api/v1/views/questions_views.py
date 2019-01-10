dc017dc743fe94a860d6499ebb4af8bbe1801321
misocho <misochobrian@gmail.com>
2019-01-10 08:49:31 +0300

    [feature #163038374] adds post question views

--- app/api/v1/views/questions_views.py
+++ app/api/v1/views/questions_views.py
@@ -3,3 +3,18 @@ from ..models import question_model
 
 question_blueprint = Blueprint('question_blueprint', __name__, url_prefix='/api/v1')
 questions = question_model.QuestionModels()
+
+@question_blueprint.route('/questions', methods=['POST'])
+def post_question():
+    """ endpoint for posting question """
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"message" : "Data set cannot be empty"})
+    meetup_id = data.get('meetup_id')
+    postedby = data.get('postedby')
+    body = data.get('body')
+
+    res = jsonify(questions.post_question(meetup_id, postedby, body))
+    res.status_code = 201
+    return res
