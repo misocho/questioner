4c184c6a7fe98e4a165155291cecf86ac37d0a48
misocho <misochobrian@gmail.com>
2019-01-10 08:52:52 +0300

    [feature #163038374] adds post question blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .api.v1.views.meetup_views import meetup_blueprint
+from .api.v1.views.questions_views import question_blueprint
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(meetup_blueprint)
+    app.register_blueprint(question_blueprint)
     return app
