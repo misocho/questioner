fb85c395982f15598ebd64bc541a7a0f5edd32d9
misocho <misochobrian@gmail.com>
2019-01-12 00:17:59 +0300

    [feature #163149110] adds updates base model

--- app/api/v1/models/base_model.py
+++ app/api/v1/models/base_model.py
@@ -1,15 +1,7 @@
-meetups = []
-rsvp = []
-questions = []
-
 
 class BaseModels(object):
     """ contains methods common to other models """
 
-    def __init__(self):
-        self.meetups  = meetups
-        self.rsvp = rsvp
-        self.questions = questions
 
     def check_item(self, item, key, db):
         data = [record for record in db if record[key] == item]
