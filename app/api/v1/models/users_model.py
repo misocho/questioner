d0208851faeb75a3ff26d758cf14381998caec05
misocho <misochobrian@gmail.com>
2019-01-12 21:46:44 +0300

    [feature #163166506] adds user init method

--- app/api/v1/models/users_model.py
+++ app/api/v1/models/users_model.py
@@ -4,6 +4,10 @@ from .base_model import BaseModels
 class UserModels(BaseModels):
     """ contains methods for user models """
 
+    def __init__(self, db):
+        self.db = 'user'
+
+
     def signup_user(self, first_name, last_name, account_type, username, email, password):
         """ methon to signup user """
         payload = {
@@ -15,4 +19,5 @@ class UserModels(BaseModels):
             "password": password
         }
 
+        self.check_db()
         self.save_data(payload)
