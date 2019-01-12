04e8a015549bc5b338e2ab4280f4a61cf4a4311a
misocho <misochobrian@gmail.com>
2019-01-12 20:09:41 +0300

    [start #163166506] adds user db to base model

--- app/api/v1/models/base_model.py
+++ app/api/v1/models/base_model.py
@@ -1,8 +1,22 @@
+users_list = []
+
 
 class BaseModels(object):
     """ contains methods common to other models """
 
+    def __init__(self, db):
+        self.db = db
+        self.user_db = users_list
+
+    def check_db(self):
+        if self.db == 'user':
+            db = self.user_db
+            return db
 
     def check_item(self, item, key, db):
         data = [record for record in db if record[key] == item]
         return data
+
+    def save_data(self, **kwargs):
+        self.db = self.check_db()
+        self.db.append(kwargs)
