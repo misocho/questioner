64b758e2d4fef23c962434b0cff8a88c9bcc9f6f
misocho <misochobrian@gmail.com>
2019-01-12 22:48:54 +0300

    [feature #163166506] fix register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .api.v1.views.meetup_views import meetup_blueprint
 from .api.v1.views.questions_views import question_blueprint
+from .api.v1.views.user_views import user_blueprint
+
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(meetup_blueprint)
     app.register_blueprint(question_blueprint)
+    app.register_blueprint(user_blueprint)
     return app
