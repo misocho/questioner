fb414806b32676d4aa62b9bf81ef66d34e8da727
misocho <misochobrian@gmail.com>
2019-01-13 18:08:22 +0300

    [chore #163177066] adds refactore base model

--- app/api/v1/models/base_model.py
+++ app/api/v1/models/base_model.py
@@ -1,5 +1,7 @@
 users_list = []
 questions_list = []
+meetups_list = []
+rsvp_list = []
 
 
 class BaseModels(object):
@@ -15,10 +17,13 @@ class BaseModels(object):
         elif self.db == 'questions':
             db = questions_list
             return db
+        elif self.db == 'meetups':
+            db = meetups_list
+            return db
 
-    def check_item(self, item, key, db):
-        data = [record for record in db if record[key] == item]
-        return data
+        elif self.db == 'rsvp':
+            db = rsvp_list
+            return db
 
     def search_db(self, key, item):
         db = self.check_db()
@@ -28,6 +33,12 @@ class BaseModels(object):
         else:
             return False
 
+    def search_meetup(self, key, item):
+        data = [record for record in meetups_list if record[key] == item]
+        if data:
+            return data[0]
+        else:
+            return False
     def save_data(self, payload):
         db = self.check_db()
         db.append(payload)
