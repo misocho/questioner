ff4249eda336ad32f340e3d237310173d642c383
misocho <misochobrian@gmail.com>
2019-01-13 12:00:58 +0300

    [feature #163166483] adds signin user model

--- app/api/v1/models/users_model.py
+++ app/api/v1/models/users_model.py
@@ -1,4 +1,4 @@
-from .base_model import BaseModels
+from .base_model import BaseModels, users_list
 
 
 class UserModels(BaseModels):
@@ -6,7 +6,6 @@ class UserModels(BaseModels):
 
     def __init__(self):
         self.db = 'user'
-
 
     def signup_user(self, first_name, last_name, account_type, username, email, password):
         """ methon to signup user """
@@ -17,7 +16,16 @@ class UserModels(BaseModels):
             "username": username,
             "email": email,
             "password": password
-        } , {"message" : 'sign up was successfull'}
+        }
 
-        self.check_db()
         return self.save_data(payload)
+
+    def singin_user(self, username, password):
+        """ method to signin user """
+        data = self.search_db("username", username)
+        if data:
+            if data["password"] == password:
+                return {'message':'signin was successfull'}
+            return {"message" : "invalid username or password"}
+        return {"message" : "user {} does not exist".format(username)}
+       
