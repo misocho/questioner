788946f196d7bf74fbdbe80c1a40261660608cd0
misocho <misochobrian@gmail.com>
2019-01-13 11:59:49 +0300

    [start #163166483] adds search db method

--- app/api/v1/models/base_model.py
+++ app/api/v1/models/base_model.py
@@ -1,4 +1,4 @@
-users_list = {"userdb":[]}
+users_list = []
 
 
 class BaseModels(object):
@@ -9,12 +9,19 @@ class BaseModels(object):
 
     def check_db(self):
         if self.db == 'user':
-            db = users_list["userdb"]
+            db = users_list
             return db
+
 
     def check_item(self, item, key, db):
         data = [record for record in db if record[key] == item]
         return data
+
+
+    def search_db (self, key, item):
+        db = self.check_db()
+        data = [record for record in db if record[key] == item]
+        return data[0]
 
     def save_data(self, payload):
         db = self.check_db()
