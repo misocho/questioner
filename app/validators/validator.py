c353eea1e4908adcc4ab5df11686bef3c002a567
misocho <misochobrian@gmail.com>
2019-01-15 08:51:34 +0300

    [chore #163177816] adds validations

--- app/validators/validator.py
+++ app/validators/validator.py
@@ -1,3 +1,24 @@
+import re
 """ Validate user input """
-class BaseValidations(object):
-    pass
+
+
+class BaseValidations():
+    """" Base validaions for user input """
+    """ Class method checks if email is valid """
+    @classmethod
+    def valid_email(cls, email):
+        """ Below is a regular expressoin to match an email """
+        regx = re.compile(r"[a-zA-Z0-9]+@[a-zA-Z]+\.com")
+        match = regx.finditer(email)
+        for match in match:
+            if match:
+                return True
+        return False
+
+    @classmethod
+    def input_provided(cls, key):
+        """ Class method checks if input is provided """
+        if key == "":
+            return False
+        else: 
+            return True
