fdc262da5732f6364170448562fb93d16274cfdb
misocho <misochobrian@gmail.com>
2019-01-15 09:12:11 +0300

    [chore #163177816] adds validation to questions

--- app/api/v1/views/questions_views.py
+++ app/api/v1/views/questions_views.py
@@ -1,5 +1,8 @@
 from flask import Flask, Blueprint, request, jsonify, make_response
 from ..models import question_model
+from ....validators import validator
+
+question_validation = validator.BaseValidations()
 
 question_blueprint = Blueprint('question_blueprint', __name__, url_prefix='/api/v1')
 questions = question_model.QuestionModels()
@@ -13,7 +16,7 @@ def post_question():
         data = request.get_json() if request.is_json else None
     except Exception:
         return jsonify({"message": "data not in json"}), 400
-        
+
     data = request.get_json()
     if not data:
         return jsonify({"message" : "Data set cannot be empty"}), 202
@@ -21,6 +24,8 @@ def post_question():
     postedby = data.get('postedby')
     body = data.get('body')
 
+    if not question_validation.input_provided(body):
+        return jsonify ({"message" : "Please provide question body"}) , 400
     res = jsonify(questions.post_question(meetup_id, postedby, body))
     res.status_code = 201
     return res
