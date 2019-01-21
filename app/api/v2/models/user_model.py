3bdf7c305f8316f394758b3620e12174367c7100
misocho <misochobrian@gmail.com>
2019-01-21 09:42:32 +0300

    [feature #163267425] adds user sigin views

--- app/api/v2/models/user_model.py
+++ app/api/v2/models/user_model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from ....database.db_con import connect
+from psycopg2.extras import RealDictCursor
 
 
 class Users:
@@ -15,33 +16,23 @@ class Users:
         if isAdmin == None:
             isAdmin = False
 
-        cursor = self.db.cursor()
+        cursor = self.db.cursor(cursor_factory=RealDictCursor)
         query = """ INSERT INTO users (firstname, lastname, othername, email, phoneNumber,
-                username, password, isAdmin) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING registered"""
+                username, password, isAdmin) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING * """
 
         cursor.execute(query, (firstname, lastname, othername, email, phoneNumber, username, password, isAdmin))
-        registered = cursor.fetchone()[0]
+        user = cursor.fetchone()
         self.db.commit()
         cursor.close()
 
 
-        user = {
-            "firstname" : firstname,
-            "lastname" : lastname,
-            "othername" : othername,
-            "email" : email,
-            "phonenumber" : phoneNumber,
-            "username" : username,
-            "password" : password,
-            "isAdmin" : isAdmin,
-            "registered" : registered
-        }
-        
         return user
 
     def signin(self, userdata, username):
         """ creates user signin model """
 
+        cursor = self.db.cursor(cursor_factory=RealDictCursor)
         query = " SELECT {} FROM users WHERE username = '{}' ".format(userdata, username)
-
-        return query
+        cursor.execute(query)
+        data = cursor.fetchone()
+        return data
--- app/api/v2/views/user_view.py
+++ app/api/v2/views/user_view.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, make_response, request
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from ..models.user_model import Users
 
@@ -16,7 +16,7 @@ def signup():
     except:
         return jsonify({
             "status" : 400,
-            "message" : "Invalid input"
+            "message" : "Data not in json"
         }), 400
 
     firstname = data.get('firstname')
@@ -28,11 +28,55 @@ def signup():
     isAdmin = data.get('isAdmin')
     password = data.get('password')
 
-    password = generate_password_hash(password)
+    password = generate_password_hash(password, )
 
     res = user.signup(firstname, lastname, othername, email, phoneNumber, username, password, isAdmin)
     
-    return jsonify(res, {
+    return jsonify({
+        "data" : [res],
         "status" : 201,
         "message" : "registration was successful"
     }) , 201
+
+@auth.route('/signin', methods=['POST'])
+def sigin():
+    try:
+        data = request.get_json()
+    
+    except:
+        return jsonify({
+            "status" : 400,
+            "message" : "Data not in json"
+        })
+
+    username = data.get('username')
+    password = data.get('password')
+
+    userdata = "id, firstname, lastname, username, password, email"
+
+
+    data = user.signin(userdata, username)
+
+    if data:
+        check = check_password_hash( data['password'], password)
+        if check:
+            return jsonify({
+                "status" : 200,
+                "message" : "Successfully signed is as {}".format(data['username'])
+            }) , 200
+
+        else:
+
+            return jsonify({
+                "status" : 401,
+                "message" : "Invalid password"
+            }) , 401
+
+    else:
+        return jsonify({
+            "status" : 404,
+            "message" : "User {} does not exist".format(username)
+        }) , 404
+    
+
+    
