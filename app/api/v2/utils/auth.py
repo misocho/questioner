dc677323583f89e05fa1c162a0363e78169fd6fb
misocho <misochobrian@gmail.com>
2019-01-23 00:20:59 +0300

    [chore #163375779] fix login decorator

--- app/api/v2/utils/auth.py
+++ app/api/v2/utils/auth.py
@@ -2,7 +2,7 @@ import jwt
 import os
 from functools import wraps
 from datetime import datetime, timedelta
-from flask import request
+from flask import request, jsonify
 from app.api.v2.models.user_model import Users
 
 
@@ -19,6 +19,41 @@ class Auth:
             'session': session
         }
 
-        token = jwt.encode(token_details, os.getenv("SECRET_KEY"), algorithm='HS256')
+        token = jwt.encode(token_details, os.getenv(
+            "SECRET_KEY"), algorithm='HS256').decode('utf-8')
 
         return str(token)
+
+
+def login_required(f):
+    """ User authentication decorator """
+    @wraps(f)
+    def authenticate(*args, **kwargs):
+        """ decorator for login authentication """
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].encode('ascii', 'ignore')
+            try:
+                data = jwt.decode(
+                    token, os.getenv("SECRET_KEY"),
+                    algorithm='HS256'
+                )
+            except jwt.ExpiredSignatureError:
+                return jsonify({
+                    'status': 401,
+                    'error': 'Your session has expired. Please login again'
+                }), 401
+            user = data['username']
+
+            if user:
+                return f(*args, **kwargs, current_user=user)
+            return jsonify({
+                "status": 404,
+                "error": "User not found"
+            }), 404
+
+        else:
+            return jsonify({
+                "status": 401,
+                "error": "You are not logged in. Please login"
+            }), 401
+    return authenticate
