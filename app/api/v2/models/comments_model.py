18bf24dd201987fce519c4f7ebe492d5f2b4128a
misocho <misochobrian@gmail.com>
2019-01-31 12:53:53 +0300

    [chore #163369139] refactors questions and comments models

--- app/api/v2/models/comments_model.py
+++ app/api/v2/models/comments_model.py
@@ -1,8 +1,8 @@
 from app.database.db_con import connect
 from psycopg2.extras import RealDictCursor
 from app.api.v2.models.questions_model import Questions
+from app.database.db_con import QuestionerDB
 
-question = Questions()
 
 class Comments:
     """ contains methods for comments models """
@@ -10,21 +10,16 @@ class Comments:
     def add_comment(self, username, question_id, comment):
         """ contains model for posting a question """
 
-        db = connect()
-
-        cursor = db.cursor(cursor_factory=RealDictCursor)
-
-        search_question = question.getOne(question_id, "id")
-
         query = "SELECT title, body FROM questions WHERE id = {}".format(
             question_id)
 
-        cursor.execute(query)
-        question_data = cursor.fetchone()
+        question_data = QuestionerDB.fetch_one(query)
 
-        query_2 = """INSERT INTO comments (question_id, question_title, question_body, comment, username) VALUES (%s, %s, %s, %s, %s) RETURNING * """
+        query_2 = """INSERT INTO comments (question_id, question_title,
+        question_body, comment, username) VALUES (%s, %s, %s, %s, %s)
+        RETURNING * """
 
-        cursor.execute(
-            query_2, (question_id, question_data['title'], question_data['body'], comment, username))
-        comment = cursor.fetchone()
-        return comment
+        data = (question_id, question_data['title'], question_data['body'],
+                comment, username)
+        
+        return QuestionerDB.save(query_2, data)
