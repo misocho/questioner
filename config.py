80004395c4acc25f5eb5d742e3c96a7480327adc
misocho <misochobrian@gmail.com>
2019-02-03 17:30:00 +0300

    update config

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Development(Config):
 class Production(Config):
     DEBUG = False
     TESTING = False
-    DB_URL = os.environ['DATABASE_URL']
+    DB_URL = os.environ['DATABASE_URL'] = "postgres://qlzqbdvkiettzv:a6a43d2550377fb9932fe640fe517ef2d2937cc6d6ddc5897e539a9a16ebdd89@ec2-23-21-244-254.compute-1.amazonaws.com:5432/d1rl3g8596kh7m"
 
 
 class Testing(Config):
