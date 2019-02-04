bf148c2eec4255259f4b4600440d2910cab37196
misocho <misochobrian@gmail.com>
2019-02-04 13:48:34 +0300

    [feature #163681226] consume signup user api

--- UI/static/signup.js
+++ UI/static/signup.js
@@ -1,40 +1,39 @@
 // Create a new user
 var signUp = document.getElementById('signup-btn');
-function register(event){
-    let firstname = document.getElementById('firstname')
-    let lastname = document.getElementById('lastname')
-    let othername = document.getElementById('othername')
-    let email = document.getElementById('email')
-    let phoneNumber = document.getElementById('phonenumber')
-    let username = document.getElementById('username')
-    let password = document.getElementById('password')
+function register(event) {
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let othername = document.getElementById('othername').value;
+    let email = document.getElementById('email').value;
+    let phoneNumber = document.getElementById('phonenumber').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
     fetch('https://misocho01-questioner.herokuapp.com/api/v2/auth/signup', {
-        method:'POST',
+        mode: 'cors',
+        method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'Content-type': 'aoolication/json'
+            'ContentType': 'application/json'
         },
         body: JSON.stringify({
             firstname: firstname,
             lastname: lastname,
+            othername: othername,
             email: email,
             phoneNumber: phoneNumber,
             username: username,
             password: password
         })
     })
-    .then((resp) => resp.json())
-    .then((data) => {
-        if (data.status == 200){
-            window.location.href = 'index.html'
-            window.alert(data.data)
-        }else{
-            window.alert(data.error);
-        }
-    })
-    .catch((error) => console.error(error))
+        .then((resp) => resp.json())
+        .then((data) => {
+            if (data.status == 200) {
+                window.location.href = 'index.html'
+                window.alert(data.data)
+            } else {
+                window.alert(data.error);
+            }
+        })
+        .catch((error) => console.error(error))
 }
-documentTitle = document.querySelector('title').innerHTML;
-if (documentTitle == "Questioner" ){
-    signUp.addEventListener('click', register);
-}
+signUp.addEventListener('click', register);
