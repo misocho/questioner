160c706782793facaa94c68a28379f1c4326b828
misocho <misochobrian@gmail.com>
2019-02-04 18:10:37 +0300

    [finishes #163698007] consumes singin endpoint

---
+++ UI/static/signin.js
@@ -0,0 +1,29 @@
+// Sign in user
+var signIn = document.getElementById('signin-btn');
+function login(event) {
+    event.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    fetch('https://misocho01-questioner.herokuapp.com/api/v2/auth/signin', {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            if (data.status == 200) {
+                window.location.href = '../templates/index.html'
+            } else {
+                window.alert(data.error);
+            }
+        })
+        .catch((error) => console.error(error))
+}
+signIn.addEventListener('click', login);
