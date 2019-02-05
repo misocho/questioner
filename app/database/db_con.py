17f95d58f1c7fb351f028f131653d8020682378f
misocho <misochobrian@gmail.com>
2019-02-05 16:25:00 +0300

    [finishes #163725344] creates default admin

--- app/database/db_con.py
+++ app/database/db_con.py
@@ -3,7 +3,7 @@ from flask import current_app
 from app.database import migrations
 import os
 from psycopg2.extras import RealDictCursor
-
+from werkzeug.security import generate_password_hash
 
 class QuestionerDB:
     """ DB connection class """
@@ -25,6 +25,7 @@ class QuestionerDB:
                 cls.cursor.execute(query)
             cls.con.commit()
             print("Tables created successfully in PostgreSQL ")
+            cls.create_admin()
         except (Exception, psycopg2.DatabaseError) as error:
             print("Error while creating PostgreSQL table", error)
 
@@ -37,7 +38,6 @@ class QuestionerDB:
             cls.con.commit()
         except (Exception, psycopg2.DatabaseError) as error:
             print("Error while dropping PostgreSQL table", error)
-
 
     @classmethod
     def save(cls, query, data):
@@ -78,3 +78,28 @@ class QuestionerDB:
         data = cls.cursor.fetchone()
         return data
 
+    @classmethod
+    def create_admin(cls):
+        """ checks if an admin user exists """
+        query = "SELECT * FROM users WHERE isAdmin=true"
+        admin = cls.fetch_one(query)
+        if not admin:
+            firstname = "admin"
+            lastname = "admin"
+            othername = "admin"
+            email = "questioneradmin@gmail.com"
+            phoneNumber = "254798724968"
+            username = "admin"
+            password = generate_password_hash("@Admin1254")
+            isAdmin = "True"
+
+            query_2 = """ INSERT INTO users (firstname, lastname, othername, email, phoneNumber,
+                username, password, isAdmin) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING firstname, lastname, othername, email, phoneNumber,
+                username, isAdmin"""
+
+            data = (firstname, lastname, othername, email, phoneNumber,
+                    username, password, isAdmin)
+
+            cls.save(query_2, data)
+            
+            print("Admin user created")
