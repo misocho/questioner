bcba0fc5d9f0f3d5780606000621b7409177a706
misocho <misochobrian@gmail.com>
2019-02-06 18:40:18 +0300

    [feature

--- UI/static/create_meetup.js
+++ UI/static/create_meetup.js
@@ -1,3 +1,17 @@
+function changeDateFormat(inputDate){  // expects Y-m-d
+    var splitDate = inputDate.split('-');
+    if(splitDate.count == 0){
+        return null;
+    }
+
+    var year = splitDate[0];
+    var month = splitDate[1];
+    var day = splitDate[2]; 
+
+    return month + '-' + day + '-' + year;
+}
+
+
 // Create meetup
 var createMeetup = document.getElementById('submit-btn');
 function create(event) {
@@ -5,7 +19,7 @@ function create(event) {
     let title = document.getElementById('title').value;
     let organizer = document.getElementById('organizer').value;
     let location = document.getElementById('location').value;
-    let date = document.getElementById('date').value;
+    let date = changeDateFormat(document.getElementById('date').value);
     let time = document.getElementById('time').value;
 
     fetch('https://misocho01-questioner.herokuapp.com/api/v2/meetups', {
@@ -24,10 +38,15 @@ function create(event) {
     })
         .then((resp) => resp.json())
         .then((data) => {
-            if (data.status !== 200){
+            if (data.status !== 201) {
                 console.log(data.error)
             }
+            else {
+                console.log(data.message)
+            }
         })
+    var happeningOn2 = date + " " + time;
+    console.log(happeningOn2);
 }
 
 createMeetup.addEventListener('click', create)
