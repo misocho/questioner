eaedc799b2b824d00fd2edc1dbfe081e9db02206
misocho <misochobrian@gmail.com>
2019-02-08 15:41:44 +0300

    [feature #163817947] adds scripts for image upload

---
+++ UI/static/upload_image.js
@@ -0,0 +1,22 @@
+var inputImage = document.getElementById('upload');
+function previewImage(event) {
+    event.preventDefault();
+    let reader = new FileReader();
+    let uploadField = document.getElementById('upload-field')
+    let ImageField = document.getElementById("image-field");
+
+    reader.onload = function(){
+        if(reader.readyState == 2){
+            ImageField.src = reader.result;
+            ImageField.style.width = "100%";
+            uploadField.style.display = "none";
+            console.log(reader.result);
+        }
+    }
+
+    reader.readAsDataURL(event.target.files[0]);
+    
+    
+}
+
+inputImage.addEventListener('change', previewImage);
