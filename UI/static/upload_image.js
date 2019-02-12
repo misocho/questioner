694f02f45a14e6ab9eb16030ea2fdcd2b019ea79
misocho <misochobrian@gmail.com>
2019-02-12 23:38:51 +0300

    [feature #163817947] hosts uploaded images on imgur

--- UI/static/upload_image.js
+++ UI/static/upload_image.js
@@ -4,14 +4,18 @@ function previewImage(event) {
     let reader = new FileReader();
     let uploadField = document.getElementById('upload-field')
     let ImageField = document.getElementById("image-field");
+    let imageContainer = document.getElementById("img-container");
+    
 
     reader.onload = function(){
         if(reader.readyState == 2){
             ImageField.src = reader.result;
             ImageField.style.width = "100%";
             uploadField.style.display = "none";
-            let image = reader.result;
+            ImageField.style.height = "276px";
+            imageContainer.style.display = "contents";
         }
+        console.log(reader.result)
     }
 
     reader.readAsDataURL(event.target.files[0]);
@@ -20,3 +24,24 @@ function previewImage(event) {
 }
 
 inputImage.addEventListener('change', previewImage);
+
+var save = document.getElementById('save-btn');
+function uploadImgur(event) {
+    event.preventDefault();
+    let image = document.getElementById('image-field').src;
+    var strImage = image.replace(/^data:image\/[a-z]+;base64,/, "");
+    var image_url;
+    data = new FormData();
+    data.append("image", strImage);
+    fetch('https://api.imgur.com/3/image', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Client-ID 6c78ded85463291'
+        },
+        body: data
+    }).then((resp) => resp.json())
+        .then((data) => {
+            image_url = data.data.link;
+        })
+}
+save.addEventListener('click', uploadImgur);
