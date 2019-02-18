536652da5560a2af2c3ce94a87b7f516c056e008
misocho <misochobrian@gmail.com>
2019-02-18 20:11:53 +0300

    [finishes #163817947] saves image imgur link to database

--- UI/static/create_meetup.js
+++ UI/static/create_meetup.js
@@ -9,7 +9,7 @@ function changeDateFormat(inputDate) {  // expects Y-m-d
     var year = splitDate[0];
     var month = splitDate[1];
     var day = splitDate[2];
-
+ 
     return month + '-' + day + '-' + year;
 }
 
