3066c3d744b7c7c12120d009cf44cb62e69a52c7
misocho <misochobrian@gmail.com>
2019-02-19 13:52:05 +0300

    [feature #163736344] adds meetup image thumbnails

--- UI/static/meetups.js
+++ UI/static/meetups.js
@@ -1,10 +1,5 @@
 function Meetup() {
-    let url = 'https://misocho01-questioner.herokuapp.com/api/v2/meetups';
-    let title = document.getElementById('title');
-    let organizer = document.getElementById('organizer');
-    let location = document.getElementById('location');
-    let date = document.getElementById('date');
-    let time = document.getElementById('time');
+    let url = 'https://misocho01-questioner.herokuapp.com/api/v2/meetups/upcoming';
 
     fetch(url, {
         method: 'GET',
@@ -15,10 +10,62 @@ function Meetup() {
 
         .then(response => response.json())
         .then((data) => {
-            console.log(data);
+            let meetup = data.data[0];
+            let meetupHtml = '';
+            console.log(meetup[1]);
+            let image_url = '';
+            for (let i = 0; i < meetup.length; i += 1) {
+                console.log(meetup[i]);
+                let date = meetup[i].happeningon
+                let imageThumbnail = ''
+                var splitDate = date.split(' ');
+                console.log(splitDate)
+                month = splitDate[2];
+                date = splitDate[1];
+                if (meetup[i].images == null){
+                    imageThumbnail = "../images/dafault_image.svg";
+                } else {
+                    url = 'https://i.imgur.com/'
+                    image_url = meetup[i].images.replace(url, '');
+                    file = image_url.split('.');
+                    fileName = file[0] + 'm';
+                    fileExtension = file[1];
+                    imageThumbnail = url + fileName +'.'+ fileExtension;
+                    console.log(imageThumbnail);
+                }
+
+                meetupHtml += ` <div class="card">
+                <div class="in-card">
+                    <div class="in-card-upper">
+                        <div class="in-card-image">
+                        <image src="${imageThumbnail}"></image>
+                        </div>
+                    </div>
+                    <div class="lower-container">
+                        <div class="in-card-lower">
+                            <div class="in-card-date">
+                                <div class="month">${month}</div>
+                                <div class="date">${date}</div>
+                            </div>
+                            <div class="in-card-text">
+                                <div class="meetup-text">
+                                    <div class="meetup-title">
+                                        <a href="questions.html">${meetup[i].title}</a>
+                                    </div>
+                                    <div class="meetup-details">
+                                        <p>${meetup[i].happeningon}</p>
+                                        <p>${meetup[i].location}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+            }
+            document.getElementById('meetups').innerHTML = meetupHtml;
         }
 
         )
 }
-
 window.addEventListener('load', Meetup);
